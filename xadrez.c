4b0744dc46268dbf61bb76b410d897c1c4c2dbb1
Udson Silva <udson2112@gmail.com>
2025-11-01 01:23:32 +0000

    Desafio Nível Mestre

--- xadrez.c
+++ xadrez.c
@@ -4,35 +4,90 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
+void movimenta_torre(int casas, char direcao) {
+    if(casas > 0) {
+        switch (direcao)
+        {
+        case 'b':
+            printf("Baixo\n");
+            break;
+        case 'c':
+            printf("Cima\n");
+            break;
+        case 'd':
+            printf("Direita\n");
+            break;
+        case 'e':
+            printf("Esquerda\n");
+            break;
+        default:
+            printf("Direção inválida!");
+            return;
+        }
+
+        movimenta_torre(casas - 1, direcao);
+    }
+}
+
+void movimenta_bispo(int casas, char dir_ver, char dir_hor) {
+    if(casas > 0) {
+        if((dir_ver == 'b' || dir_ver == 'c') && (dir_hor == 'd' || dir_hor == 'e')) {
+            switch (dir_ver)
+            {
+            case 'b':
+                printf("Baixo, ");
+                break;
+            case 'c':
+                printf("Cima, ");
+                break;
+            }
+            switch (dir_hor)
+            {
+            case 'd':
+                printf("Direita\n");
+                break;
+            case 'e':
+                printf("Esquerda\n");
+                break;
+            }
+        } else {
+            printf("Direção inválida!");
+            return;
+        }
+        movimenta_bispo(casas - 1, dir_ver, dir_hor);
+    }
+}
+
+void movimenta_rainha (int casas, char dir_ver, char dir_hor) {
+    if(dir_ver == dir_hor) {
+        movimenta_torre(casas, dir_ver);
+    } else {
+        movimenta_bispo(casas, dir_ver, dir_hor);
+    }
+}
+
+
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-    const int casas_torre = 5, casas_bispo = 5, casas_rainha = 8, casas_cavalo[2] = {1, 2};
+    const int casas_cavalo[2] = {1, 2};
     int j = 0, k = 0;
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-    printf("---| Movimentação do Bispo |---\n");
-    for(int i = 0; i < casas_bispo; i++) {
-        printf("Cima, Direita\n");
-    }
+    printf("\n\n---| Movimentação do Bispo |---\n");
+    movimenta_bispo(5, 'c', 'd');
 
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
     printf("\n\n---| Movimentação da Torre |---\n");
-    while(j < casas_torre)
-    {
-        printf("Direita\n");
-        j++;
-    }
+    movimenta_torre(5, 'd');
+    
 
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
     printf("\n\n---| Movimentação da Rainha |---\n");
-    do {
-        printf("Esquerda\n");
-        k++;
-    } while(k < casas_rainha);
+    movimenta_rainha(8, 'e', 'e'),
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
